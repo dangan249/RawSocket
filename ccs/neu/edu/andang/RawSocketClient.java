a46eb7b8326f6ab5bfc67fbabfb182b45c9d5ccc
AN DANG <dang.an249@gmail.com>
2013-10-31 23:33:48 -0400

    creating structure - WARNING: codes not working right now

--- ccs/neu/edu/andang/RawSocketClient.java
+++ ccs/neu/edu/andang/RawSocketClient.java
@@ -1,5 +1,6 @@
 package ccs.neu.edu.andang ;
 
+import java.net.URL ;
 import java.net.InetAddress ;
 import java.net.SocketException ;
 import java.io.IOException ;
@@ -9,18 +10,96 @@ import static com.savarese.rocksaw.net.RawSocket.PF_INET;
 
 public class RawSocketClient{
 
-	RawSocket rSock = new RawSocket() ;
+	private RawSocket rSockSender ;
+	private RawSocket rSockReceiver ;
 
-	public void close() throws IOException {
-		this.rSock.close() ;
-	}
+/* TCP functionalities supported:
+
+Packet = IP Header + TCP Header + Data
+
+-- Verify the checksums of incoming TCP packets
+-- Generate correct checksums for outgoing packets.
+-- Select a valid local port to send traffic on
+-- Perform the three-way handshake
+-- Handle connection teardown. 
+-- Handle sequence and acknowledgement numbers. 
+-- Manage the advertised window as you see fit. 
+-- Include basic timeout functionality: 
+==> if a packet is not ACKed within 1 minute, assume the packet is lost and retransmit it. 
+-- Able to receive out-of-order incoming packets and put them back into the correct order
+-- Identify and discard duplicate packets. 
+-- Implement a basic congestion window: 
+==> start with cwnd=1, 
+==> increment the cwnd after each succesful ACK, up to a fixed maximum of 1000 
+-- If your program observes a packet drop or a timeout, reset the cwnd to 1.
+
+*/
+    private String remoteHost ;
+    private int remotePort ;
+
+    // TODO: set up the sender and receiver raw socks
+    public SocketClient( String remoteHost, int remotePort ){
+		this.host = host ;
+		this.port = port ;
+		// ...
+    }
+
+    // TODO: handling TCP teardown process
+    public void disconnect(){
+		try{
+			// ...
+			this.rSockSender.close() ;
+			this.rSockReceiver.close() ;
+		}
+		catch(IOException ex){
+			System.out.println( "Unable to disconnect: " + ex.toString() ) ;
+		}
+    }
+    
+    // TODO: connect to the remote server + doing the handshake
+    public boolean connect() throws UnknownHostException, SocketException, IOException{
+
+    	this.rSock = new RawSocket () ;
+		this.rSock.open( PF_INET, RawSocket.getProtocolByName("tcp")) ;
+
+		URL destURL = new URL( this.remoteHost ) ;
+
+  	    InetAddress address = InetAddress.getByName( destURL.getHost() );
+
+  	    if( address.isAnyLocalAddress() || address.isLoopbackAddress()
+  	     || address.isLinkLocalAddress() ){
+  	    	return false ;
+  	    }
+
+  	    // ...
+
+  	    return true ;
+    }
+    
+   
+    // send the message to the remote server that we connect with
+    // return: the InputStream from the server
+    public InputStream sendMessage( String message ) throws IOException{
+
+    }	
+
+    boolean isIPSupported(){
+    	return client.rSockSender.getIPHeaderInclude() ;
+    } 
+
+
+
+
+
+
+
+
+
+/*
 	public static void main( String args[] ){
 		
 		RawSocketClient client = new RawSocketClient() ;
 		try{
-			client.rSock.open( PF_INET, RawSocket.getProtocolByName("tcp")) ;
-			System.out.println( client.rSock.getIPHeaderInclude() ) ;
-			client.close() ;
 
 		}
 		catch (SocketException ex){
@@ -30,4 +109,5 @@ public class RawSocketClient{
 			System.out.println( ex.toString() ) ;
 		}
 	}
+	*/
 }
